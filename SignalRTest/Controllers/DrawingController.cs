e22e4091f838f858a491759ab52ca5498f592c13
Luis Vieira <luis.m.s.vieira@outlook.pt>
2019-05-16 23:47:16 +0100

    Removed bin and obj. Added aws settings. New endpoint

---
+++ SignalRTest/Controllers/DrawingController.cs
@@ -0,0 +1,24 @@
+﻿using DrawWars.Aws;
+using Microsoft.AspNetCore.Mvc;
+using SignalRTest.Models;
+using System;
+
+namespace SignalRTest.Controllers
+{
+    [Route("api/drawing")]
+    public class DrawingController : Controller
+    {
+        [HttpPost]
+        public string Submit([FromBody] PictureUploadModel model)
+        {
+            //TODO Validate model.SessionID and model.PlayerID as 
+
+            var filename = $"{model.SessionID}{model.PlayerID}.{model.Extension}";
+            var picture = Convert.FromBase64String(model.Drawing);
+
+            var uri = new AwsManager().S3_UploadFile(filename, picture);
+
+            return uri;
+        }
+    }
+}
---
+++ SignalRTest/Models/PictureUploadModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SignalRTest.Models
+{
+    public class PictureUploadModel
+    {
+        [Required]
+        public Guid SessionID { get; set; }
+
+        [Required]
+        public Guid PlayerID { get; set; }
+
+        [Required]
+        public string Drawing { get; set; }
+
+        [Required]
+        public string Extension { get; set; }
+    }
+}
