5459060080853e50554f025666343dd0c881e9cd
Luis Vieira <luis.m.s.vieira@outlook.pt>
2019-09-08 21:40:28 +0100

    .

---
+++ DrawWars.Database/dbo/Tables/DrawWarsUser.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[DrawWarsUser] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Username] VARCHAR (256)  NULL,
+    [PassHash] VARCHAR (1024) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ DrawWars.Database/dbo/Tables/UserDevice.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[UserDevice] (
+    [UserId]     INT              NOT NULL,
+    [DeviceUuid] UNIQUEIDENTIFIER NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC, [DeviceUuid] ASC),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[DrawWarsUser] ([Id])
+);
+
